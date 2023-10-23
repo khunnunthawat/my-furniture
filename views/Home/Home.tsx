34929aba2a5fb203cb9ba25b1ba0d1323c631f46
khunnunthawat <khunnunthawat@gmail.com>
2023-10-24 00:46:52 +0700

    ui-product-card-responsive

--- views/Home/Home.tsx
+++ views/Home/Home.tsx
@@ -23,7 +23,7 @@ const rangType = [
 export default function Home() {
   return (
     <>
-      {/* <div className="relative mb-14 flex h-[716px] w-full items-center justify-end bg-interior bg-cover bg-center">
+      <div className="relative mb-14 flex h-[716px] w-full items-center justify-end bg-interior bg-cover bg-center">
         <div className="container flex md:absolute md:justify-center lg:justify-end xl:static xl:right-6">
           <div className="bg-secondary px-10 pb-6 pt-8 md:pb-9 md:pt-16">
             <div className="flex max-w-[674px] flex-col items-start">
@@ -71,45 +71,47 @@ export default function Home() {
             </div>
           ))}
         </div>
-      </div> */}
+      </div>
       <div className="container my-14">
         <div className="flex w-full flex-col items-center justify-center">
           <h1 className="mb-8 text-center text-[40px] font-bold text-gray-500">
             Our Products
           </h1>
-          <ul className="grid grid-cols-4 gap-8">
-            <li className="overflow-hidden">
-              <div className="group relative h-[446px] w-full min-w-[285px]">
-                <Image
-                  width={285}
-                  height={301}
-                  src={"/images/syltherine.svg"}
-                  alt=""
-                  className="object-cover"
-                />
-                <div className="absolute left-1/2 top-1/2 z-10 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center transition duration-300 group-hover:opacity-100">
-                  <Button variant="outline" className="mb-6 w-[200px]">
-                    Outline
-                  </Button>
-                  <div className="inline-flex cursor-pointer space-x-5 text-white">
-                    <div className="inline-flex items-center">
-                      <ShareIcon className="mr-1" />
-                      Share
-                    </div>
-                    <div className="inline-flex items-center">
-                      <CompareIcon className="mr-1" />
-                      Compare
-                    </div>
-                    <div className="inline-flex items-center">
-                      <FavoriteIcon className="mr-1" />
-                      Like
+          <ul className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-8">
+            {Array.from(Array(8).keys()).map((_, idx: number) => (
+              <li className="overflow-hidden" key={idx}>
+                <div className="group relative h-[301px] w-full min-w-[285px]">
+                  <Image
+                    width={285}
+                    height={301}
+                    src={"/images/syltherine.svg"}
+                    alt=""
+                    className="object-cover"
+                  />
+                  <div className="absolute left-1/2 top-1/2 z-10 flex w-full -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center px-4 opacity-0 transition duration-300 group-hover:opacity-100">
+                    <Button variant="outline" className="mb-6 w-[200px]">
+                      Outline
+                    </Button>
+                    <div className="inline-flex space-x-5 text-white">
+                      <div className="inline-flex cursor-pointer items-center">
+                        <ShareIcon className="mr-1 fill-current" />
+                        Share
+                      </div>
+                      <div className="inline-flex cursor-pointer items-center">
+                        <CompareIcon className="mr-1 fill-current" />
+                        Compare
+                      </div>
+                      <div className="inline-flex cursor-pointer items-center">
+                        <FavoriteIcon className="mr-1" />
+                        Like
+                      </div>
                     </div>
                   </div>
+                  <div className="absolute left-0 top-0 h-full w-full bg-gray-500 bg-opacity-70 opacity-0 transition duration-500 group-hover:opacity-100" />
                 </div>
-                <div className="absolute left-0 top-0 h-full w-full bg-gray-500 bg-opacity-70 transition duration-500 group-hover:opacity-100" />
                 <div className="h-[145px] bg-gray-white p-4">
                   <div className="flex flex-col">
-                    <div className="mb-2 text-2xl font-semibold text-gray-500">
+                    <div className="mb-2 cursor-pointer text-2xl font-semibold text-gray-500 hover:text-primary-500">
                       Syltherine
                     </div>
                     <div className="font-medium text-gray-300">
@@ -125,8 +127,8 @@ export default function Home() {
                     </div>
                   </div>
                 </div>
-              </div>
-            </li>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
