d24cb02530dc779c76350351d292886f5d44aa2f
khunnunthawat <khunnunthawat@gmail.com>
2023-10-28 22:59:06 +0700

    ui-rooms-inspiration

--- views/Home/Home.tsx
+++ views/Home/Home.tsx
@@ -1,3 +1,4 @@
+import { Button } from "../../components/ui";
 import Banner from "./Banner";
 import Content from "./Content";
 import OurProduct from "./OurProduct";
@@ -8,6 +9,31 @@ export default function Home() {
       <Banner />
       <Content />
       <OurProduct />
+      <div className="mb-14 flex h-[670px] w-full flex-row items-center bg-secondary">
+        <div className="mx-auto max-w-md pr-8">
+          <h1 className="mb-1.5 whitespace-pre-line text-4xl font-bold text-gray-500">
+            {`50+ Beautiful rooms\ninspiration`}
+          </h1>
+          <p className="mb-6">
+            {`Our designer already made a lot of beautiful\n prototipe of rooms that
+            inspire you`}
+          </p>
+          <Button className="min-w-[176px]">Explore More</Button>
+        </div>
+      </div>
     </>
   );
 }
+
+// .container-left {
+//   width: 100%;
+//   margin-right: auto;
+//   margin-left: auto;
+//   padding-right: 2rem/* 32px */;
+//   padding-left: 2rem/* 32px */;
+// }
+// @media (min-width: 1400px) {
+//   .container {
+//     max-width: 1400px;
+//   }
+// }
