3105e7c0698d93d4cfdd3151a142a8315cb864d7
khunnunthawat <khunnunthawat@gmail.com>
2023-11-17 14:22:23 +0700

    test-deploy

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,16 @@
 // import { Banner, Content, OurProduct } from "../../views/home";
 
-import Banner from "../../views/home/Banner";
-import Content from "../../views/home/Content";
-import OurProduct from "../../views/home/OurProduct";
+// import Banner from "../../views/home/Banner";
+// import Content from "../../views/home/Content";
+// import OurProduct from "../../views/home/OurProduct";
 import RoomInspiration from "../../views/home/RoomInspiration";
 
 export default function HomePage() {
   return (
     <>
-      <Banner />
+      {/* <Banner />
       <Content />
-      <OurProduct />
+      <OurProduct /> */}
       <RoomInspiration />
     </>
   );
