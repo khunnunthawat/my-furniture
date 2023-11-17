396704a80ec6e11470b4917e173d7cd7fe611a7f
khunnunthawat <khunnunthawat@gmail.com>
2023-11-17 12:42:51 +0700

    section-home

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,4 @@
-import { Home } from "../../views/home";
+import Home from "@/views/home/Home";
 
 export default function HomePage() {
   return <Home />;
--- views/Home/index.tsx
+++ views/Home/index.tsx
@@ -1,7 +1,6 @@
 import Banner from "./Banner";
-import Home from "./Home";
 import Content from "./Content";
 import OurProduct from "./OurProduct";
 import RoomInspiration from "./RoomInspiration";
 
-export { Banner, Home, Content, OurProduct, RoomInspiration };
+export { Banner, Content, OurProduct, RoomInspiration };
