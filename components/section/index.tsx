b79c6f67bcfe7c6b4eac8878330ad20a9a5bf807
khunnunthawat <khunnunthawat@gmail.com>
2023-11-17 15:22:32 +0700

    test-deploy-2

--- components/section/index.tsx
+++ components/section/index.tsx
@@ -1,4 +1,4 @@
-import Banner from "./Banner";
+import Banner from "./banner";
 import Content from "./content";
 import OurProduct from "./our-product";
 import RoomInspiration from "./room-inspiration";
