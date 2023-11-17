bd02cff0e0bfdab244e022821f0fa6d29a2e39f7
khunnunthawat <khunnunthawat@gmail.com>
2023-11-17 14:20:59 +0700

    test-deploy

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,8 @@
-import { Banner, Content, OurProduct } from "../../views/home";
+// import { Banner, Content, OurProduct } from "../../views/home";
+
+import Banner from "../../views/home/Banner";
+import Content from "../../views/home/Content";
+import OurProduct from "../../views/home/OurProduct";
 import RoomInspiration from "../../views/home/RoomInspiration";
 
 export default function HomePage() {
--- views/Home/index.tsx
+++
@@ -1,5 +0,0 @@
-import Banner from "./Banner";
-import Content from "./Content";
-import OurProduct from "./OurProduct";
-
-export { Banner, Content, OurProduct };
