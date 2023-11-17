3155b35d072c03a22b1b152c6ad4b4df91bc6785
khunnunthawat <khunnunthawat@gmail.com>
2023-11-17 12:44:37 +0700

    section-home

--- views/Home/index.tsx
+++ views/Home/index.tsx
@@ -1,6 +1,7 @@
+import Home from "./Home";
 import Banner from "./Banner";
 import Content from "./Content";
 import OurProduct from "./OurProduct";
 import RoomInspiration from "./RoomInspiration";
 
-export { Banner, Content, OurProduct, RoomInspiration };
+export { Home, Banner, Content, OurProduct, RoomInspiration };
